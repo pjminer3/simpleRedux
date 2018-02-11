7997c7bc981e6bbc91bac92e257df482463efecc
Patrick Miner <patrickminer318@gmail.com>
2018-02-11 12:59:50 -0800

    Added BooksReducer to our book app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// need to export the reducer so it can be used in other parts of the project
+export default function () {
+  return [
+    { title: 'Javascript' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ];
+}
+
